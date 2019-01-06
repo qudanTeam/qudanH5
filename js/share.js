b98ff1fd5ecea151e823fddeb59012e720dcadcd
谷瑞廷 <guruiting@guruitingdeAir.lan>
2019-01-06 23:12:52 +0800

    bug修改

--- js/share.js
+++ js/share.js
@@ -14,9 +14,15 @@ function wxConfig(Data) {
     timestamp: Data.data.timestamp, // 必填，生成签名的时间戳
     nonceStr: Data.data.noncestr, // 必填，生成签名的随机串
     signature: Data.data.signature,// 必填，签名
-    jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData'] // 必填，需要使用的JS接口列表
+    jsApiList: ['onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareTimeline', 'onMenuShareQZone'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
   });
- 
+  wx.checkJsApi({
+    jsApiList: ['onMenuShareAppMessage', 'onMenuShareQQ', 'onMenuShareTimeline', 'onMenuShareQZone'], // 需要检测的JS接口列表，所有JS接口列表见附录2,
+    success: function (res) {
+      // 以键值对的形式返回，可用的api值true，不可用为false
+      // 如：{"checkResult":{"chooseImage":true},"errMsg":"checkJsApi:ok"}
+    }
+  });
   wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
     if (ptype == 1) {
       link = protocol + '//' + host + '/applyCardShare.html?id=' + id + '&shareid=' + document.body.getAttribute("data-shareid")// 
@@ -24,32 +30,33 @@ function wxConfig(Data) {
     if (ptype == 2) {
       link = protocol + '//' + host + '/applyLoanShare.html?id=' + id + '&shareid=' + document.body.getAttribute("data-shareid")// 
     }
-    
+
     var shareData = {
       title: detail.product.productName, // 分享标题
       desc: detail.shares[0].content, // 分享描述
       link: link, // 分享链接，该链接域名或路径必须与当前页面对应的公众号JS安全域名一致
       imgUrl: detail.product.logo, // 分享图标
       success: function () {
-        layerMsg("设置成功")
+        // layerMsg("设置成功")
         // 设置成功
-        // var data = JSON.stringify({
-        //   shareid: document.body.getAttribute("data-shareid")
-        // });
+        var data = JSON.stringify({
+          shareid: document.body.getAttribute("data-shareid")
+        });
         // layerMsg(shareid)
-        // Ajax_token(IP + 'msqudan/api/user/share/addcounturl', "POST", token, data, addShareCount);//增加用户分享次数
+        Ajax_token(IP + 'msqudan/api/user/share/addcounturl', "POST", token, data, addShareCount);//增加用户分享次数
       },
       cancel: function () {
         // 用户取消分享后执行的回调函数
       }
     };
-    
     //自定义“分享给朋友”及“分享到QQ”按钮的分享内容
-    wx.updateAppMessageShareData(shareData);
+    wx.onMenuShareAppMessage(shareData);
+    wx.onMenuShareQQ(shareData);
     // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
-    wx.updateTimelineShareData(shareData);
-    
-    
+    wx.onMenuShareTimeline(shareData);
+    wx.onMenuShareQZone(shareData);
+
+
   });
   wx.error(function (res) {
     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
@@ -62,8 +69,8 @@ function getDetail(Data) {
     detail = Data.data.detail;
     //设置商品海报图片
     var proPoster = document.querySelector(".content-poster-share");
-    if(proPoster){
-      document.querySelector(".content-poster-share").style.backgroundImage = 'url('+detail.product.productPoster+')';
+    if (proPoster) {
+      document.querySelector(".content-poster-share").style.backgroundImage = 'url(' + detail.product.productPoster + ')';
     }
   }
 }
