d360e76e7ace5fbf631d78c249d94653c3905034
谷瑞廷 <guruiting@guruitingdeAir.lan>
2019-01-06 11:48:21 +0800

    bug修改

--- js/share.js
+++ js/share.js
@@ -10,7 +10,7 @@ shareid = document.body.getAttribute("data-shareid");
 function wxConfig(Data) {
  
   wx.config({
-    debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
+    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: Data.data.appId, // 必填，公众号的唯一标识
     timestamp: Data.data.timestamp, // 必填，生成签名的时间戳
     nonceStr: Data.data.noncestr, // 必填，生成签名的随机串
