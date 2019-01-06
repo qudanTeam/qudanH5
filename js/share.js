144dc73843cd29bb131cecbf7639d9bc025d3b4f
谷瑞廷 <guruiting@guruitingdeAir.lan>
2019-01-06 11:39:43 +0800

    bug修改

--- js/share.js
+++ js/share.js
@@ -5,8 +5,10 @@ var host = window.location.host;
 var id = getQueryString('id');
 var ptype = getQueryString('ptype');
 var detail;
+
 shareid = document.body.getAttribute("data-shareid");
 function wxConfig(Data) {
+ 
   wx.config({
     debug: false, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
     appId: Data.data.appId, // 必填，公众号的唯一标识
@@ -15,6 +17,7 @@ function wxConfig(Data) {
     signature: Data.data.signature,// 必填，签名
     jsApiList: ['updateAppMessageShareData', 'updateTimelineShareData'] // 必填，需要使用的JS接口列表
   });
+
   wx.ready(function () {   //需在用户可能点击分享按钮前就先调用
     if (ptype == 1) {
       link = protocol + '//' + host + '/applyCardShare.html?id=' + id + '&shareid=' + document.body.getAttribute("data-shareid")// 
@@ -22,6 +25,7 @@ function wxConfig(Data) {
     if (ptype == 2) {
       link = protocol + '//' + host + '/applyLoanShare.html?id=' + id + '&shareid=' + document.body.getAttribute("data-shareid")// 
     }
+    
     var shareData = {
       title: detail.product.productName, // 分享标题
       desc: detail.shares[0].content, // 分享描述
@@ -38,10 +42,13 @@ function wxConfig(Data) {
         // 用户取消分享后执行的回调函数
       }
     };
+    
     //自定义“分享给朋友”及“分享到QQ”按钮的分享内容
     wx.updateAppMessageShareData(shareData);
     // 自定义“分享到朋友圈”及“分享到QQ空间”按钮的分享内容
     wx.updateTimelineShareData(shareData);
+    layerMsg(link+"<br/>"+detail.shares[0].content)
+    
   });
   wx.error(function (res) {
     // config信息验证失败会执行error函数，如签名过期导致验证失败，具体错误信息可以打开config的debug模式查看，也可以在返回的res参数中查看，对于SPA可以在这里更新签名。
