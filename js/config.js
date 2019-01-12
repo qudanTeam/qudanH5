298d0e76a3a7c01ed0a83c91072babcba991276f
谷瑞廷 <guruiting@guruitingdeAir.lan>
2019-01-12 20:55:51 +0800

    bug修改

--- js/config.js
+++ js/config.js
@@ -1,4 +1,5 @@
 var  IP = 'http://47.99.242.122:8763/';
+// var  IP = 'http://msqudan.myhshop.top/';
 var domain_test='http://www.myhshop.top/';
 var domain_test_server='http://msqudan.myhshop.top/';
 var path_local = 'Documents/qudan/H5/';
