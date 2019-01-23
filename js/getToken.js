339c7c84671e061f580eb2b34e598eab949ce276
谷瑞廷 <guruiting@guruitingdeAir.lan>
2019-01-23 20:51:05 +0800

    微信内token丢失情况，微信授权登录，再次获取token

--- js/getToken.js
+++ js/getToken.js
@@ -9,3 +9,13 @@ if (ua.match(/MicroMessenger/i) == "micromessenger") {
     window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx5937eccb3b4a99d7&redirect_uri=" + REDIRECT_URI + "&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
   }
 } 
+//获取cookie
+function getCookie(cname) {
+  var name = cname + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i].trim();
+    if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+  }
+  return "";
+}
