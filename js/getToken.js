5063146edcf2280c2d531534c8acc690a000c8ca
谷瑞廷 <guruiting@guruitingdeAir.lan>
2019-01-24 13:57:47 +0800

    授权获取token问题

--- js/getToken.js
+++ js/getToken.js
@@ -1,18 +1,21 @@
-// 微信内cookie存储数据有丢失的情况，所以在获取token的时候，如果cookie里取不到，
-// 就用微信授权的scope=snsapi_base的授权登录方式授权并获取token，还是没有token的话，再跳转登录页面。
-
-token = getCookie('token');
-var ua = navigator.userAgent.toLowerCase();
-if (ua.match(/MicroMessenger/i) == "micromessenger") {
-  if (token == '') {
-    var url = location.href.split('#').toString();
-    
-    var REDIRECT_URI = encodeURIComponent(domain_test + 'blankGetToken.html?type=1&isLoginAuthorize=0');
-    window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appid+"&redirect_uri=" + REDIRECT_URI + "&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
-    flagGetToken = 1;
-    // alert(flagGetToken)
+var flagGetToken = function () {
+  if (getCookie("flagGetToken")) {
+    return 1;
+  } else {
+    return 0;
   }
-} 
+};
+var token = getCookie('token');
+var ua = navigator.userAgent.toLowerCase();
+if ((flagGetToken == 0) && (ua.match(/MicroMessenger/i) == "micromessenger") && (token == '')) {
+  // if (token == ''){
+  var REDIRECT_URI = encodeURIComponent(domain_test + 'blankGetToken.html?type=1&isLoginAuthorize=0&flagGetToken=1');
+  window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid=" + appid + "&redirect_uri=" + REDIRECT_URI + "&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
+  flagGetToken = 1;
+  // }
+} else {
+  flagGetToken = 1;
+}
 //获取cookie
 function getCookie(cname) {
   var name = cname + "=";
