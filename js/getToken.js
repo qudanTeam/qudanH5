87294fcd82088ce5c6ecf0f7bcf2b69c30fc0b62
谷瑞廷 <guruiting@guruitingdeAir.lan>
2019-01-24 10:35:39 +0800

    bug修改

--- js/getToken.js
+++ js/getToken.js
@@ -10,7 +10,7 @@ if (ua.match(/MicroMessenger/i) == "micromessenger") {
     var REDIRECT_URI = encodeURIComponent(domain_test + 'blankGetToken.html?type=1&isLoginAuthorize=0');
     window.location.href = "https://open.weixin.qq.com/connect/oauth2/authorize?appid="+appid+"&redirect_uri=" + REDIRECT_URI + "&response_type=code&scope=snsapi_base&state=123#wechat_redirect";
     flagGetToken = 1;
-    alert(flagGetToken)
+    // alert(flagGetToken)
   }
 } 
 //获取cookie
