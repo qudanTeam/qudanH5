beb67e7119b13b7593f6f76f8729c8e4b5c08d3a
guruiting <757300052@qq.com>
2019-01-30 21:20:47 +0800

    修改域名配置文件

--- js/config.js
+++ js/config.js
@@ -1,6 +1,5 @@
 // var  IP = 'http://47.99.242.122:8763/';
-var  IP = 'http://msqudan.myhshop.top/';
-var domain_test='http://www.myhshop.top/';//测试环境
-var domain_test_server='http://msqudan.myhshop.top/';//接口域名
-var path_local = 'Documents/qudan/H5/';
+var  IP = 'http://msqudan.myhshop.top/';//接口域名
+var domain_test='http://www.myhshop.top/';//微信jssdk回调域名
+var domain_test_server='http://msqudan.myhshop.top/';//接口域名（微信jssdk相关）
 var appid = 'wx17dd8d8d9de5910e';
