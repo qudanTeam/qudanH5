cb59b44b79d2d2379bd4cf21181274155a2b6031
guruiting <757300052@qq.com>
2019-01-31 13:56:24 +0800

    修改jssdk回调域名为正式域名

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // var  IP = 'http://47.99.242.122:8763/';
 var  IP = 'http://txmsqudan.myhshop.top/';//接口域名
-var domain_test='http://h5test.myhshop.top/';//微信jssdk回调域名
+var domain_test='http://www.myhshop.top/';//微信jssdk回调域名
 var domain_test_server='http://txmsqudan.myhshop.top/';//接口域名（微信jssdk相关）
 var appid = 'wx17dd8d8d9de5910e';
