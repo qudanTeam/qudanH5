3b18319f0d166b87d10a880976091904c21deaf5
DESKTOP-MGK0F9E\A-013 <chenfg@hadlinks.com>
2019-02-13 22:49:38 +0800

    测试版本1.0

--- js/config.js
+++ js/config.js
@@ -1,6 +1,8 @@
-// var  IP = 'http://47.99.242.122:8763/';
-var  IP = 'http://msqudan.myhshop.top/';
-var domain_test='http://www.myhshop.top/';//测试环境
-var domain_test_server='http://msqudan.myhshop.top/';//接口域名
+var  IP = 'http://txmsqudan.myhshop.top/';
+var domain_test='http://txmsqudan.myhshop.top/';//测试环境
+var domain_test_server='http://txmsqudan.myhshop.top/';//接口域名
+//var  IP = 'http://msqudan.myhshop.top/';
+//var domain_test='http://www.myhshop.top/';//测试环境
+//var domain_test_server='http://msqudan.myhshop.top/';//接口域名
 var path_local = 'Documents/qudan/H5/';
 var appid = 'wx17dd8d8d9de5910e';
