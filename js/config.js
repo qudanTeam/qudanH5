83a91919c714b10b9299ec536d91a5f0c3f4667b
DESKTOP-MGK0F9E\A-013 <chenfg@hadlinks.com>
2019-02-14 19:18:20 +0800

    修改测试环境地址bug

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 var  IP = 'http://txmsqudan.myhshop.top/';
-var domain_test='http://txmsqudan.myhshop.top/';//测试环境
+var domain_test='http://h5test.myhshop.top/';//测试环境
 var domain_test_server='http://txmsqudan.myhshop.top/';//接口域名
 //var  IP = 'http://msqudan.myhshop.top/';
 //var domain_test='http://www.myhshop.top/';//测试环境
