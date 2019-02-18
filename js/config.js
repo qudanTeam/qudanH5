1a5ab05da27262ceaad52e25f68b6ed163ecab94
DESKTOP-MGK0F9E\A-013 <chenfg@hadlinks.com>
2019-02-18 22:26:54 +0800

    正式版本构建

--- js/config.js
+++ js/config.js
@@ -1,9 +1,9 @@
-var  IP = 'http://txmsqudan.myhshop.top/';
-var domain_test='http://h5test.myhshop.top/';//测试环境
-var domain_test_server='http://txmsqudan.myhshop.top/';//接口域名
-var appid="wx5937eccb3b4a99d7";
-//var  IP = 'http://msqudan.myhshop.top/';
-//var domain_test='http://www.myhshop.top/';//测试环境
-//var domain_test_server='http://msqudan.myhshop.top/';//接口域名
-//var appid = 'wx17dd8d8d9de5910e';
+//var  IP = 'http://txmsqudan.myhshop.top/';
+//var domain_test='http://h5test.myhshop.top/';//测试环境
+//var domain_test_server='http://txmsqudan.myhshop.top/';//接口域名
+//var appid="wx5937eccb3b4a99d7";
+var  IP = 'http://msqudan.myhshop.top/';
+var domain_test='http://www.myhshop.top/';//正式环境
+var domain_test_server='http://msqudan.myhshop.top/';//接口域名
+var appid = 'wx17dd8d8d9de5910e';
 var path_local = 'Documents/qudan/H5/';
