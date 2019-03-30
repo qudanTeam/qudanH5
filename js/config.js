d4671f49a87ce004b709d0f1e387d7b27aa6fc5f
exg <liuzhi@moore.ren>
2019-03-30 21:27:07 +0800

    增加趣单生活的配置

--- js/config.js
+++ js/config.js
@@ -1,18 +1,18 @@
+var path_local = 'Documents/qudan/H5/';
 //var  IP = 'http://txmsqudan.myhshop.top/';
 //var domain_test='http://h5test.myhshop.top/';//测试环境
 //var domain_test_server='http://txmsqudan.myhshop.top/';//接口域名
 //var appid="wx5937eccb3b4a99d7";
 
 //趣单正式环境
-var  IP = 'http://msqudan.myhshop.top/';
-var domain_test='http://www.myhshop.top/';//正式环境
-var domain_test_server='http://msqudan.myhshop.top/';//接口域名
-var appid = 'wx17dd8d8d9de5910e';
-var path_local = 'Documents/qudan/H5/';
+//var  IP = 'http://msqudan.myhshop.top/';
+//var domain_test='http://www.myhshop.top/';//正式环境
+//var domain_test_server='http://msqudan.myhshop.top/';//接口域名
+//var appid = 'wx17dd8d8d9de5910e';
+
 //趣单生活正式环境
 var  IP = 'http://msqudan.myhshop.wang/';
 var domain_test='http://www.myhshop.wang/';//正式环境
 var domain_test_server='http://msqudan.myhshop.wang/';//接口域名
 var appid = 'wx5937eccb3b4a99d7';
-var path_local = 'Documents/qudan/H5/';
 
